bf1fb301cd5e5d6f47d98b62d879e9c373982256
Taina <tainadomingueslemes@gmail.com>
2024-03-05 14:56:35 -0300

    exercicios

---
+++ aula4/ex2.py
@@ -0,0 +1,19 @@
+n1= int (input('Digite um número'))
+n2= int (input('Digite outro número'))
+op= str (input('Digite um operador'))
+
+if op == "+" :
+    som = n1 + n2
+    print('A soma desses números é:',som)
+
+elif op == "-" :
+    sub = n1 - n2
+    print('A subtração desses números é:',sub)
+
+elif op == "*" or op =="x":
+    mult = n1 * n2
+    print('A multiplicção desses números é:',mult)
+
+elif op == "/" :
+    div = n1 / n2
+    print('A divisão desses números é:',div)
---
+++ aula4/ex3.py
@@ -0,0 +1,25 @@
+while True :
+    n1= int (input('Digite um número'))
+    n2= int (input('Digite outro número'))
+    op= (input('Digite um operador'))
+
+    if op == "+" :
+        som = n1 + n2
+        print('A soma desses números é:',som)
+
+    elif op == "-" :
+        sub = n1 - n2
+        print('A subtração desses números é:',sub)
+
+    elif op == "*" or op =="x":
+        mult = n1 * n2
+        print('A multiplicção desses números é:',mult)
+
+    elif op == "/" :
+        div = n1 / n2
+        print('A divisão desses números é:',div)
+
+    resp = (input('Você deseja continuar (sim ou não)'))
+
+    if resp == "não":
+        break
